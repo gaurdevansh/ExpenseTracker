2421e5fe21d716f015baa672b761cf3530fee154
Devansh <devanshgaur75@gmail.com>
2024-02-05 23:51:57 +0530

    Added 3 fragments, Bottom Navigation View and Navigation Graph for navigation

--- app/src/main/java/com/example/expensetracker/MainActivity.kt
+++ app/src/main/java/com/example/expensetracker/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.expensetracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.expensetracker.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var navController = findNavController(R.id.navController)
+        var bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        bottomNav.setupWithNavController(navController)
+
     }
 }
