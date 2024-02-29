605092608b8984f2ea20e0e5074cfb132ea50fc7
Devansh <devanshgaur75@gmail.com>
2024-02-29 23:47:08 +0530

    Implemented pie chart view with round end segments

--- app/src/main/java/com/example/expensetracker/graph/PieChartView.kt
+++ app/src/main/java/com/example/expensetracker/graph/PieChartView.kt
@@ -2,30 +2,31 @@ package com.example.expensetracker.graph
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.RectF
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import com.example.expensetracker.R
+import kotlin.math.cos
 import kotlin.math.min
+import kotlin.math.sin
 
-class PieChartView(context: Context, attrs: AttributeSet): View(context, attrs) {
+class PieChartView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
     private val paint = Paint().apply {
         isAntiAlias = true
         style = Paint.Style.STROKE
-        strokeWidth = 100f
+        strokeWidth = 80f
     }
 
-    private val colors = arrayOf(Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW, Color.CYAN)
-    private val data = floatArrayOf(20f, 30f, 15f, 10f, 25f)
+    //private val colors = arrayOf(Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW, Color.CYAN)
+    private val colors = arrayOf(resources.getColor(R.color.dark_green), resources.getColor(R.color.dark_yellow), resources.getColor(R.color.dark_brown))
+    private val data = floatArrayOf(20f, 30f, 50f)
 
     @SuppressLint("CanvasSize")
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        if(canvas == null) return
+        if (canvas == null) return
 
         val width = canvas.width.toFloat()
         val height = canvas.height.toFloat()
@@ -34,34 +35,38 @@ class PieChartView(context: Context, attrs: AttributeSet): View(context, attrs) 
         val centerY = height / 2
         val total = data.sum()
 
-        var startAngle = -90f
+        var startAngle = -70f
+        var gapAngle = 12f
 
-        for((index, value) in data.withIndex()) {
+        val semiCircleRadius = paint.strokeWidth * 0.1f - 7.5f
+
+
+        for ((index, value) in data.withIndex()) {
             val sweepAngle = 360f * (value / total)
             paint.color = colors[index % colors.size]
 
-            val gapAngle = 10f
-            val gapStartAngle = startAngle + gapAngle / 2
-            val gapEndAngle = startAngle + sweepAngle - gapAngle / 2
-            val gapRadius = radius + paint.strokeWidth / 2
-            val semiCircleRadius = gapAngle / 2
-            val semiCircleRect = RectF(centerX + radius, centerY + gapRadius + paint.strokeWidth/2, centerX + radius + paint.strokeWidth, centerY + gapRadius)
 
             //Main Arc
-            canvas.drawArc(centerX - radius, centerY - radius, centerX + radius, centerY + radius
-                , startAngle + gapAngle/2, sweepAngle-gapAngle, false, paint)
+            canvas.drawArc(
+                centerX - radius, centerY - radius, centerX + radius, centerY + radius,
+                startAngle + gapAngle , sweepAngle - gapAngle, false, paint
+            )
 
-            //Arc around the start of the gap
-//            canvas.drawArc(centerX - gapRadius, centerY - gapRadius, centerX + gapRadius,  centerY + gapRadius,
-//            gapStartAngle, gapAngle / 2, false, paint)
-            canvas.drawArc(semiCircleRect, 0f, 180f, false, paint)
-            //Arc around the end of the gap
-//            canvas.drawArc(centerX - gapRadius, centerY - gapRadius, centerX + gapRadius,  centerY + gapRadius,
-//            gapEndAngle - gapAngle / 2, gapAngle / 2, false, paint)
-            //canvas.drawArc(semiCircleRect, startAngle + sweepAngle - semiCircleRadius, semiCircleRadius, false, paint)
+            val startAngleRadians = Math.toRadians(startAngle.toDouble() + gapAngle.toDouble())
+            val startX = centerX + radius * cos(startAngleRadians).toFloat()
+            val startY = centerY + radius * sin(startAngleRadians).toFloat()
+
+            //Semicircle at start
+            canvas.drawArc(startX - semiCircleRadius, startY - semiCircleRadius, startX + semiCircleRadius, startY + semiCircleRadius,
+                startAngle + gapAngle , -180f, false, paint)
+
+            val endX = centerX + radius * cos(Math.toRadians(startAngle + sweepAngle.toDouble())).toFloat()
+            val endY = centerY + radius * sin(Math.toRadians(startAngle + sweepAngle.toDouble())).toFloat()
+            //Semicircle at end
+            canvas.drawArc(endX - semiCircleRadius, endY - semiCircleRadius, endX + semiCircleRadius, endY + semiCircleRadius,
+            startAngle + sweepAngle, 180f, false, paint)
+
             startAngle += sweepAngle
         }
-
     }
-
 }
