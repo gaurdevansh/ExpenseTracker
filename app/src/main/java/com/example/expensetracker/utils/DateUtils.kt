fe2dde91ba11d218a9c7cf7959985f97f9f9dc40
Devansh <devanshgaur75@gmail.com>
2024-03-05 23:13:21 +0530

    Added method for fetching date according to week, month and year

--- app/src/main/java/com/example/expensetracker/utils/DateUtils.kt
+++ app/src/main/java/com/example/expensetracker/utils/DateUtils.kt
@@ -1,5 +1,6 @@
 package com.example.expensetracker.utils
 
+import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.Locale
@@ -12,5 +13,19 @@ object DateUtils {
 
     fun getDateInFormat2(date: LocalDate): String {
         return date.format(DateTimeFormatter.ofPattern("d MMM, YY", Locale.ENGLISH))
+    }
+
+    fun getStartPeriod(currentDate: LocalDate, timeFrame: TimeFrame): LocalDate {
+        return when(timeFrame) {
+            TimeFrame.WEEK -> {
+                currentDate.with(DayOfWeek.MONDAY)
+            }
+            TimeFrame.MONTH -> {
+                currentDate.withDayOfMonth(1)
+            }
+            TimeFrame.YEAR -> {
+                currentDate.withYear(1)
+            }
+        }
     }
 }
